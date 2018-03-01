706a67bcf7c2a448c9dadd458cf2ef171af3f842
Nima Izadi <nima.izadi@shopify.com>
2018-03-01 17:52:16 -0500

    Added UNIX date timestamp

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -22,6 +22,15 @@ class Conference
 
   validates :name, :url, :startDate, presence: true
 
+  def date
+    return 0 unless startDate
+    if startDate.length == 10
+      Date.parse(startDate).to_time.to_i
+    else
+      Date.parse("#{startDate}-01").to_time.to_i
+    end
+  end
+
   def id
     Digest::SHA1.base64digest "#{url}-#{startDate}"
   end
@@ -29,6 +38,7 @@ class Conference
   def as_json(*args)
     super(*args).merge(
       objectID: id
+      date: date,
     )
   end
 end
