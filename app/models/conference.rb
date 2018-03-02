23a668e8d06de4ec4cec64656f3889abbeb1759f
Nima Izadi <nima.izadi@shopify.com>
2018-03-02 10:39:37 -0500

    Added cfpDate to conferences

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -28,10 +28,16 @@ class Conference
     Date.parse(startDate.length == 10 ? startDate : "#{startDate}-01").to_time.to_i
   end
 
+  def cfpDate
+    return 0 unless cfpEndDate
+    Date.parse(cfpEndDate.length == 10 ? cfpEndDate : "#{cfpEndDate}-01").to_time.to_i
+  end
+
   def as_json(*args)
     super(*args).except(:id).merge(
       objectID: id,
       date: date,
+      cfpDate: cfpDate,
     )
   end
 end
