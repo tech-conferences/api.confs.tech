b70faf62ae14dd359b1e884b0b1d276bfeb20f0b
Nima Izadi <nim.izadi@gmail.com>
2018-03-04 20:51:03 -0500

    Added auth key in secrets for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
   attr_reader :current_user
 
   private
 
   def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    authenticated = params[:auth] == Rails.application.secrets.auth
+    render json: { error: 'Not Authorized' }, status: 401 unless authenticated
+    # @current_user = AuthorizeApiRequest.call(request.headers).result
+    # render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 end
