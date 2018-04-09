7d90af1433cffb218396fbff56c171f4f968461d
Nima Izadi <nim.izadi@gmail.com>
2018-04-08 20:05:15 -0400

    Tweaked the admin

--- app/dashboards/conference_dashboard.rb
+++ app/dashboards/conference_dashboard.rb
@@ -31,10 +31,12 @@ class ConferenceDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :topics,
-    :id,
-    :uuid,
     :name,
+    :city,
+    :country,
+    :startDate,
+    :endDate,
+    :cfpUrl,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
--- app/dashboards/topic_dashboard.rb
+++ app/dashboards/topic_dashboard.rb
@@ -8,6 +8,7 @@ class TopicDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    conferences: Field::HasMany,
     id: Field::Number,
     name: Field::String,
   }.freeze
@@ -18,6 +19,7 @@ class TopicDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :conferences,
     :id,
     :name,
   ].freeze
