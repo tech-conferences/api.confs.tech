adf576c74c0b7a785e18359ee73dff245db6c3de
Nima Izadi <nima.izadi@shopify.com>
2018-07-22 14:14:57 -0400

    Added sidekiq admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :user
   namespace :admin do
     resources :conferences
     resources :topics
     root to: "conferences#index"
+
+    authenticate :user do
+      mount Sidekiq::Web => '/sidekiq'
+    end
   end
 
   namespace :api do
