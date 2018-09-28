bf3d76671c11e372882109faf7857460cf7030b9
Nima Izadi <nima.izadi@shopify.com>
2018-09-28 16:32:59 -0400

    Strip year name automatically from conference name

--- test/controllers/conferences_controller_test.rb
+++ test/controllers/conferences_controller_test.rb
@@ -1,34 +1,66 @@
 require 'test_helper'
 
 class ConferencesControllerTest < ActionDispatch::IntegrationTest
-  # test "POST /conferences succeeds" do
-  #   post conferences_url, params: conference_params
-  #   assert_equal JSON.parse(response.body)
+  # test "POST /api/conferences succeeds" do
+  #   post api_conferences_path, params: conference_params
   #   assert_equal response.status, 200
+  #   assert_equal JSON.parse(response.body)
   # end
 
-  # test "POST /conferences fails with bad url" do
-  #   post conferences_url, params: conference_params.merge(url: 'bad-url')
+  # test "POST /api/conferences fails with bad url" do
+  #   post api_conferences_path, params: conference_params.merge(url: 'bad-url')
   #   assert_equal response.status, 422
   #   assert_equal JSON.parse(response.body)['error'] == 'topic'
   # end
 
-  test "POST /conferences fails with bad topic" do
-    post conferences_url, params: conference_params.merge(topic: 'bad-topic')
+  test "POST /api/conferences fails with bad topic" do
+    post api_conferences_path, params: conference_params.merge(topic: 'bad-topic')
     assert_equal response.status, 422
     assert_equal JSON.parse(response.body)['error'], 'topic'
   end
 
-  test "POST /conferences fails with bad dates" do
-    post conferences_url, params: conference_params.merge(topic: 'bad-topic')
+  test "POST /api/conferences fails with bad dates" do
+    post api_conferences_path, params: conference_params.merge(topic: 'bad-topic')
     assert_equal response.status, 422
     assert_equal JSON.parse(response.body)['error'], 'topic'
   end
 
-  test "POST /conferences fails if missing keys" do
-    post conferences_url, params: {name: 'Name'}
+  test "POST /api/conferences fails if missing keys" do
+    post api_conferences_path, params: {name: 'Name'}
     assert_equal response.status, 422
     assert_equal JSON.parse(response.body)['error'], 'topic'
+  end
+
+  test "#sanatize_conf_params removes year name from conf" do
+    conference_controller = Api::ConferencesController.new
+    params = conference_params.merge({name: 'Conf 2018'})
+    conference_controller.stubs(:conference_params).returns(params)
+    sanatized_params = conference_controller.send :sanatize_conf_params, params
+    assert_equal sanatized_params[:name], 'Conf'
+  end
+
+  test "#sanatize_conf_params sanatizes country for USA" do
+  conference_controller = Api::ConferencesController.new
+  params = conference_params.merge({country: 'USA'})
+  conference_controller.stubs(:conference_params).returns(params)
+  sanatized_params = conference_controller.send :sanatize_conf_params, params
+  assert_equal sanatized_params[:country], 'U.S.A.'
+end
+
+  test "#sanatize_conf_params sanatizes country for UK" do
+    conference_controller = Api::ConferencesController.new
+    params = conference_params.merge({country: 'UK'})
+    conference_controller.stubs(:conference_params).returns(params)
+    sanatized_params = conference_controller.send :sanatize_conf_params, params
+    assert_equal sanatized_params[:country], 'U.K.'
+  end
+
+  test "#sanatize_conf_params doesn't sanatizes country if correct" do
+    conference_controller = Api::ConferencesController.new
+    params = conference_params.merge({country: 'U.S.A.'})
+    conference_controller.stubs(:conference_params).returns(params)
+    sanatized_params = conference_controller.send :sanatize_conf_params, params
+    assert_equal sanatized_params[:country], 'U.S.A.'
   end
 
   private
