fbc3d6b5ffc3bc6f063ceb9a8475c91af750e70b
Nima Izadi <nim.izadi@gmail.com>
2018-10-13 12:39:18 -0400

    Configured TravisCI env for tests

--- config/initializers/algolia.rb
+++ config/initializers/algolia.rb
@@ -1,4 +1,4 @@
 Algolia.init(
-  application_id: Rails.application.secrets.algolia_id,
-  api_key: Rails.application.secrets.algolia_api_key
+  application_id: Rails.application.secrets.algolia_id || ENV['algolia_id'],
+  api_key: Rails.application.secrets.algolia_api_key || ENV['algolia_api_key']
 ) unless Rails.env.tests?
