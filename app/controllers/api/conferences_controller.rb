cf37afdb3963d3ace077674bc92a551ad52d588c
Nima <nim.izadi@gmail.com>
2021-01-17 18:51:15 +0100

    Refactored into creation service

--- app/controllers/api/conferences_controller.rb
+++ app/controllers/api/conferences_controller.rb
@@ -4,114 +4,28 @@ class Api::ConferencesController < ApiController
   before_action :validate_params, only: :create
 
   def create
-    commit_content = gh_wrapper.update(file[:content], sanatize_conf_params(conference_params))
-    commit = gh_wrapper.create_commit(commit_message, filepath, file[:sha], commit_content, branch_name)
-    result = gh_wrapper.create_pull_request(branch_name, commit_message, pr_body)
+    conference = Conferences::CreationService.run!(params: create_params)
 
-    render json: result
+    render json: conference
   end
 
   private
 
-  def gh_wrapper
-    @gh_wrapper ||= ::GithubWrapper.new
-  end
-
-  def file
-    begin
-      gh_wrapper.pull_or_create_from_repo(filepath)
-    rescue => exception
-      {
-        content: nil,
-        sha: nil
-      }
-    end
-  end
-
-  def pr_body
-    sanatize_params = sanatize_conf_params(conference_params)
-
-    <<~PRBODY
-      Hey there, it's ConfsBot! 👋🏼
-
-      Here is a new conference:
-      [#{sanatize_params[:url]}](#{sanatize_params[:url]})
-      #{sanatize_params[:cfpUrl].present? ? "CFP: [#{sanatize_params[:cfpUrl]}](#{sanatize_params[:cfpUrl]})" : ''}
-
-      ```json
-      // #{@topic}
-
-      #{JSON.pretty_generate(sanatize_params)}
-      ```
-      #{params[:comment] ? '--' : ''}
-      #{params[:comment]}
-    PRBODY
-  end
-
   def validate_topic
-    if Topic.where(name: conference_params[:topic]).length === 0
+    if Topic.where(name: create_params[:topic]).length.zero?
       render json: { error: 'topic' }, status: 422
     end
   end
 
   def validate_params
-    conf = Conference.new(conference_params.except(:topic))
+    conf = Conference.new(create_params.except(:topic))
 
     if !conf.valid?
       render json: { error: conf.errors }, status: 422
     end
   end
 
-  def commit_message
-    "Add #{params[:name]} conference"
-  end
-
-  def branch_name
-    @branch_name ||= (0...19).map { (65 + rand(26)).chr }.join.downcase
-  end
-
-  def year
-    return Date.today.year unless conference_params[:startDate]
-    conference_params[:startDate].split('-').first
-  end
-
-  def filepath
-    "conferences/#{year}/#{conference_params[:topic]}.json"
-  end
-
-  def sanatize_conf_params(params)
-    @topic = params.delete :topic
-    params.delete :cfpUrl if params[:cfpUrl].blank?
-    params.delete :cfpEndDate if params[:cfpEndDate].blank?
-    params.delete :cocUrl if params[:cocUrl].blank?
-    params.delete :twitter if params[:twitter].blank?
-    params[:name] = sanatize_name params[:name]
-    params[:country] = sanatize_country_name params[:country]
-
-    params[:url] = URLHelper.fix_url(params[:url].gsub(/\/$/, ''))
-    params[:cfpUrl] = URLHelper.fix_url(params[:cfpUrl].gsub(/\/$/, '')) if params[:cfpUrl].present?
-
-    params
-  end
-
-  def sanatize_name(name)
-    name.gsub(year, '').strip
-  end
-
-  def sanatize_country_name(country_name)
-    case country_name.downcase
-    when 'the netherlands', 'nl'
-      return 'Netherlands'
-    when 'u.s.', 'us', 'usa', 'u.s.a', 'united states', 'united states of america'
-      return 'U.S.A.'
-    when 'uk', 'uk.', 'u.k', 'uk', 'united kingdom', 'england'
-      return 'U.K.'
-    end
-
-    country_name
-  end
-
-  def conference_params
+  def create_params
     params.permit(
       :name,
       :url,
@@ -124,6 +38,7 @@ class Api::ConferencesController < ApiController
       :cfpUrl,
       :cfpEndDate,
       :cocUrl,
+      :online,
       :offersSignLanguageOrCC,
     ).to_h
   end
---
+++ app/services/conferences/creation_service.rb
@@ -0,0 +1,107 @@
+class Conferences::CreationService < ApplicationService
+  class << self
+    delegate :run!, to: :new
+  end
+
+  def run!(params:)
+    @params = params
+    sanatize_params(@params)
+
+    @gh_wrapper ||= ::GithubWrapper.new
+
+    commit_content = @gh_wrapper.update(file[:content], @params)
+    @gh_wrapper.create_commit(commit_message, filepath, file[:sha], commit_content, branch_name)
+
+    @gh_wrapper.create_pull_request(branch_name, commit_message, pr_body)
+  end
+
+  private
+
+  def sanatize_params(params)
+    @topic = params.delete :topic
+
+    params.delete(:cfpUrl) if params[:cfpUrl].blank?
+    params.delete(:cfpEndDate) if params[:cfpEndDate].blank?
+    params.delete(:cocUrl) if params[:cocUrl].blank?
+    params.delete(:twitter) if params[:twitter].blank? || params[:twitter] == '@'
+
+    params[:name] = sanatize_name(params[:name])
+    params[:country] = sanatize_country_name(params[:country]) if params[:country].present?
+
+    if params[:online] == true
+      params.delete(:country)
+      params.delete(:city)
+    end
+
+    params[:url] = URLHelper.fix_url(params[:url].gsub(/\/$/, ''))
+    params[:cfpUrl] = URLHelper.fix_url(params[:cfpUrl].gsub(/\/$/, '')) if params[:cfpUrl].present?
+
+    params
+  end
+
+  def sanatize_name(name)
+    name.gsub(year, '').strip
+  end
+
+  def sanatize_country_name(country_name)
+    case country_name.downcase
+    when 'the netherlands', 'nl'
+      return 'Netherlands'
+    when 'u.s.', 'us', 'usa', 'u.s.a', 'united states', 'united states of america'
+      return 'U.S.A.'
+    when 'uk', 'uk.', 'u.k', 'uk', 'united kingdom', 'england'
+      return 'U.K.'
+    end
+
+    country_name
+  end
+
+  def file
+    begin
+      @gh_wrapper.pull_or_create_from_repo(filepath)
+    rescue => exception
+      {
+        content: nil,
+        sha: nil
+      }
+    end
+  end
+
+  def pr_body
+    <<~PRBODY
+      Hey there, it's ConfsBot! 👋🏼
+
+      Here is a new conference:
+      [#{@params[:url]}](#{@params[:url]})
+      #{@params[:cfpUrl].present? ? "CFP: [#{@params[:cfpUrl]}](#{@params[:cfpUrl]})" : ''}
+
+      ```json
+      // #{@topic}
+
+      #{JSON.pretty_generate(@params)}
+      ```
+      #{@params[:comment] ? '--' : ''}
+      #{@params[:comment]}
+    PRBODY
+  end
+
+  def commit_message
+    "Add #{@params[:name]} conference"
+  end
+
+  # Random name to prevent branch name collision
+  def branch_name
+    @branch_name ||= (0...19).map { (65 + rand(26)).chr }.join.downcase
+  end
+
+  def year
+    puts @params
+    return Date.today.year unless @params[:startDate]
+
+    @params[:startDate].split('-').first
+  end
+
+  def filepath
+    "conferences/#{year}/#{@params[:topic]}.json"
+  end
+end
