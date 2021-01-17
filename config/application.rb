60fc06527832e90d20b2173568c1f9704e9e8d9c
Nima <nim.izadi@gmail.com>
2021-01-17 19:06:12 +0100

    Don't load Dotenv if production

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv::Railtie.load if defined?(Dotenv)
 
 module ApiConfsTech
   class Application < Rails::Application
